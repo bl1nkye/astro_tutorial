ca3439668e18c31446a15aa6fad4035396190383
FogRunner <48587219+bl1nkye@users.noreply.github.com>
2024-07-06 05:36:04 +0000

    xml works

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,15 +1,11 @@
-import rss, { pagesGloToRssItems, pagesGlobToRssItems } from "@astrojs/rss";
-import { ApiContextStorage } from "astro/actions/runtime/store.js";
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export async function GET(content) {
-    return rss (
-        {
-            title: "Astro Learner | Blog",
-            description: "My Journey learning Astro",
-            site: context.site,
-            items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-            customData: "<language>en-us</language>"
-        }
-    );
+export async function GET(context) {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
 }
-
